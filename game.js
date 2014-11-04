1302f46d85bd4f631aec048b23f518b94c69918e
bigbadwofl <skichenbrand@gmail.com>
2014-11-04 15:54:51 +0200

    sdflk

--- game.js
+++ game.js
@@ -7,8 +7,6 @@ var Game = {
     Init: function() {
         this._name = '_' + RI(1000, 9999);
 
-        I.Init();
-
         var canvas = $('canvas')[0];
 
         canvas.width = 700;
@@ -29,8 +27,12 @@ var Game = {
         $('body').on('keydown', function (event) {
             var key = String.fromCharCode(event.keyCode);
 
-            if (key == 'A')
+            if (key == 'D')
                 socket.emit('Move', Game._name + '$1|0');
+            if (key == 'A')
+                socket.emit('Move', Game._name + '$-1|0');
+            if (key == 'W')
+                socket.emit('Move', Game._name + '$0|-1');
             else if (key == 'S')
                 socket.emit('Move', Game._name + '$0|1');
         });
@@ -39,22 +41,21 @@ var Game = {
             var data = msg.split('$');
             Game.Move(data[0], data[1]);
         });
-
-        setInterval(Game.Update, 15);
     },
     Notify: function (msg) {
         var data = msg.split('$');
 
        this._players[data[0]] = new Player();
        var pos = data[1].split('|');
-       this._players[data[0]]._x = pos[0]|0;
-       this._players[data[0]]._y = pos[1]|0;
+       this._players[data[0]]._color = pos[0];
+       this._players[data[0]]._x = pos[1]|0;
+       this._players[data[0]]._y = pos[2]|0;
     },
     Connect: function (name) {
         this._players[name] = new Player();
 
         var p = this._players[this._name];
-        socket.emit('Notify', this._name + '$' + p._x + '|' + p._y);
+        socket.emit('Notify', this._name + '$' + p._color + '|' + p._x + '|' + p._y);
     }, 
     Move: function (name, data) {
         var d = data.split('|');
@@ -73,69 +74,17 @@ function Player() {
     this._x = 10;
     this._y = 10;
 
+    this._color = ['red', 'green', 'blue', 'cyan', 'magenta', 'black'][RI(5)];
+
     this.Move = function(x, y) {
         this._x += x;
         this._y += y;
     };
 
     this.Render = function() {
-        Game._ctx.fillStyle = 'red';
+        Game._ctx.fillStyle = this._color;
         Game._ctx.fillRect(this._x, this._y, 10, 10);
     };
-}
-
-var L = 'length';
-var SL = 'slice';
-var PS = 'push';
-
-var I = {
-    keys: [],
-    keyStates: [],
-    Init: function() {
-        document.body.onkeydown = this.KeyDown,
-        document.body.onkeyup = this.KeyDown
-    },
-    KeyDown: function(a, b, c, d, k, s) {
-        for (b = String.fromCharCode(a.keyCode),
-            c = "keyup" != a.type,
-            d = -1,
-            i = -1,
-            k = I.keys,
-            s = I.keyStates; ++i < k[L];)
-            if (k[i] == b) return d = i, c || (k[SL](i, 1), s[SL](i, 1));
-        c && (-1 == d ? (k[PS](b), s[PS](1)) : s[d]++)
-    },
-    Key: function(a, b, d, e, k, s) {
-        k = this.keys, s = this.keyStates;
-        for (i = 0; i < k[L]; i++) {
-            d = k[i];
-            if (d == a) {
-                e = s[i] > 0;
-                return b && (k[SL](i, 1), s[SL](i, 1)), e
-            }
-        }
-        return !1
-    },
-    Keys: function(a, b, c) {
-        for (c = [], i = 0; i < a[L]; i++) null != a[i] ? c[PS](this.CheckKeyDown(a[i], b)) : c[PS](!1);
-        return c
-    }
-};
-
-function P(a, b, c, d) {
-    return .4202 * (.7 * PN(2 * a / c, 2 * b / c) + .49 * PN(4 * a / c, 4 * b / c) + 1.19) * d
-}
-
-function PG(a, b, c) {
-    return c = ~~a + 57 * ~~b, c = c << 12 ^ c, 1 - (2147483647 & ~~(c * (60493 * c * c + 19990303) + 1376312589 + "")[SS](2, 18)) / 1073741824
-}
-
-function PN(a, b, c, d) {
-    return c = ~~a, d = ~~b, PI(PI(PG(c, d), PG(c + 1, d), a - c), PI(PG(c, d + 1), PG(c + 1, d + 1), a - c), b - d)
-}
-
-function PI(a, b, c, d) {
-    return d = (1 - MC(3.14 * c)) / 2, a * (1 - d) + b * d
 }
 
 function RF(a, b) {
@@ -144,21 +93,4 @@ function RF(a, b) {
 
 function RI(a, b) {
     return b || (b = a, a = 0), ~~RF(a, b + 1)
-}
-
-function RE(a) {
-    return a[RI(a[L] - 1)]
-}
-
-function RN(a, b, c, d) {
-    for (b || (b = a, a = 0), d = 0, c = 0; ++d < 5; c += RF(1));
-    return a + (c - 2) / 2 * b
-}
-
-function RNA(a, b) {
-    return MA(RN(a, b))
-}
-
-function RNE(a) {
-    return a[~~RNA(a[L] - 1)]
 }
